84293eba6a6690017cec4303f9116eae41d35560
hoorialaiba <hoorialaiba123@gmail.com>
2026-02-01 22:03:39 +0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IMS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
