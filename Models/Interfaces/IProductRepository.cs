fd9c1fe6609f1076d7bd562b9175bb34a0528743
hoorialaiba <hoorialaiba123@gmail.com>
2026-02-01 21:52:03 +0500

    Add files via upload

---
+++ Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace IMS.Models.Interfaces
+{
+    public interface IProductRepository
+    {
+        public List<Product> GetAll();
+        public Product GetById(int id);
+
+        public void Add(Product product);
+
+        public void Update(Product product);
+
+        public void Delete(int id);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,123 @@
+﻿namespace IMS.Models
+{
+    public class Product
+    {
+        private string name;
+        private string brand;
+        private int quantity;
+        private decimal price;
+        private string type;
+        private string expiry;
+        private string barcode;
+        private string description;
+        private IFormFile productImage;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+            set
+            {
+                brand = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        public string Expiry
+        {
+            get
+            {
+                return expiry;
+            }
+            set
+            {
+                expiry = value;
+            }
+        }
+
+        public string Barcode
+        {
+            get
+            {
+                return barcode;
+            }
+            set
+            {
+                barcode = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public IFormFile ProductImage
+        {
+            get
+            {
+                return productImage;
+            }
+            set
+            {
+                productImage = value;
+            }
+        }
+    }
+}
