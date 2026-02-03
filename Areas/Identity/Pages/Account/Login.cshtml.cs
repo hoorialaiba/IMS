00381a733d5d87dcd0fc12ef37b3924cc5805456
hoorialaiba <hoorialaiba123@gmail.com>
2026-02-03 21:15:45 +0500

    Add files via upload

---
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,98 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using IMSIdentity.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace IMSIdentity.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LoginModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return Page();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, Input.Password, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                var userClaims = await _userManager.GetClaimsAsync(user);
+                var claims = new List<Claim>(userClaims);
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
+
+                if (!claims.Any(c => c.Type == "UserType"))
+                {
+                    claims.Add(new Claim("UserType", user.UserType ?? ""));
+                }
+
+                var claimsIdentity = new ClaimsIdentity(claims, IdentityConstants.ApplicationScheme);
+                var principal = new ClaimsPrincipal(claimsIdentity);
+
+                await HttpContext.SignInAsync(IdentityConstants.ApplicationScheme, principal,
+                    new AuthenticationProperties { IsPersistent = Input.RememberMe });
+
+                if (user.UserType == "Admin")
+                {
+                    return LocalRedirect("/Admin/Dash");
+                }
+                else
+                {
+                    return LocalRedirect("/User/Udash");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return Page();
+            }
+        }
+    }
+}
